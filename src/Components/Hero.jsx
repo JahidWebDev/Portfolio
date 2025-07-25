275f5b8c0d9b1f858e5b3255f8558f564408ff68
JahidWebDev <webdeveloper2324@gmail.com>
2025-07-25 15:02:51 +0600

    project portfolio

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
     { name: "About", href: "#about" },
     { name: "Skills", href: "#skills" },
     { name: "Projects", href: "#projects" },
-    { name: "Experience", href: "#experience" },
+    { name: "Courses", href: "#experience" },
     { name: "Contact", href: "#contact" },
   ];
 
